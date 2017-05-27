96f3a97dc5e39a0e551925fda81ae7135fa5802c
Steve <steve.rjones@outlook.com>
2017-05-27 13:56:12 -0700

    Completed homework

---
+++ lesson_four.js
@@ -0,0 +1,7 @@
+document.body.style.backgroundColor = "red";
+document.getElementById("aboutMe").style.backgroundColor = "blue";
+document.body.style.fontFamily = "sans-serif";
+
+document.getElementById("nickname").innerHTML = "Stevie";
+document.getElementById("favorites").innerHTML = "Cold weather";
+document.getElementById("hometown").innerHTML = "Muncie, In";
