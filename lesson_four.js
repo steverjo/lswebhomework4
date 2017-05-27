b3e6b6cafb6ae78dc001a0becae6962c97fa9ea7
Steve <steve.rjones@outlook.com>
2017-05-27 15:06:38 -0700

    Completed extra credit

--- lesson_four.js
+++ lesson_four.js
@@ -5,3 +5,20 @@ document.body.style.fontFamily = "sans-serif";
 document.getElementById("nickname").innerHTML = "Stevie";
 document.getElementById("favorites").innerHTML = "Cold weather";
 document.getElementById("hometown").innerHTML = "Muncie, In";
+
+/* Extra Credit for loop */
+var x = document.getElementsByTagName('li');
+for(i = 0; i < x.length; i++) {
+  x[i].className = "listitem";
+  document.getElementsByClassName('listitem')[i].style.color = "red";
+}
+
+/* Extra Credit Image */
+function myPhoto() {
+  document.getElementById("me").src = "me.jpg";
+  document.getElementById('me').style.display = "block";
+  document.getElementById('me').style.margin = "0 auto";
+}
+function myPhotoGone() {
+  document.getElementById('me').style.display = "none";
+}
