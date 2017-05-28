3993382ff1caaec768d2f898d79406d8a7afb295
Steve <steve.rjones@outlook.com>
2017-05-27 17:49:32 -0700

    Final edits and function added

--- lesson_four.js
+++ lesson_four.js
@@ -1,24 +1,25 @@
-document.body.style.backgroundColor = "red";
-document.getElementById("aboutMe").style.backgroundColor = "blue";
-document.body.style.fontFamily = "sans-serif";
+function webhw4() {
+  document.body.style.backgroundColor = "red";
 
-document.getElementById("nickname").innerHTML = "Stevie";
-document.getElementById("favorites").innerHTML = "Cold weather";
-document.getElementById("hometown").innerHTML = "Muncie, IN";
+  document.getElementById("aboutMe").style.backgroundColor = "blue";
+  document.body.style.fontFamily = "sans-serif";
+
+  document.getElementById("nickname").innerHTML = "Stevie";
+  document.getElementById("favorites").innerHTML = "Cold weather";
+  document.getElementById("hometown").innerHTML = "Muncie, IN";
 
 /* Extra Credit for loop */
-var x = document.getElementsByTagName('li');
-for(i = 0; i < x.length; i++) {
-  x[i].className = "listitem";
-  document.getElementsByClassName('listitem')[i].style.color = "red";
-}
-
+  var x = document.getElementsByTagName("li");
+  for(i = 0; i < x.length; i++) {
+    x[i].className = "listitem";
+    document.getElementsByClassName("listitem")[i].style.color = "red";
+  };
+};
 /* Extra Credit Image */
 function myPhoto() {
   document.getElementById("me").src = "me.jpg";
-  document.getElementById('me').style.display = "block";
-  document.getElementById('me').style.margin = "0 auto";
-}
+  document.getElementById("me").style.display = "block";
+};
 function myPhotoGone() {
   document.getElementById('me').style.display = "none";
-}
+};
